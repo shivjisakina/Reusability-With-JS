1be5a23f55131105df6b7bc0fbf193d3bf1290e6
Sakina Shivji <shivjisakina@gmail.com>
2017-08-06 19:29:26 -0400

    created html elements in jquery

---
+++ app.js
@@ -0,0 +1,15 @@
+var app = function (first, last) {
+
+    var form = $("<form>");
+
+    var firstnameInput = $("<input>");
+        firstname = $("<label>");
+        firstname.text(first);
+
+    var lastnameInput = $("<input>");
+        lastname = $("<label>");
+        lastname.text(last);
+
+    var break1 = $("<br>");
+
+}
